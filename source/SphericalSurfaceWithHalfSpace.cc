782500a968cde5206e2c1e776680d7ea8a25cb71
cpecar <pecarc@duq.edu>
2024-07-12 13:20:28 -0400

    fix phi range for sphere IsInside check

--- source/SphericalSurfaceWithHalfSpace.cc
+++ source/SphericalSurfaceWithHalfSpace.cc
@@ -8,14 +8,15 @@ bool SphericalSurfaceWithHalfSpace::IsInside(TVector3 p) const {
   // lab frame theta, phi
   double u = p.Theta();
   double v = p.Phi();
+  if (v < 0) v += 2*M_PI;
 
   bool insideSphere;
   if (Vmin() <= Vmax()) {
     insideSphere = (u >= Umin() && u <= Umax() && v >= Vmin() && v <= Vmax());
   } else {
     insideSphere = ( (v >= Vmin() || v <= Vmax()) && (u >= Umin() && u <= Umax() ) );
-  }
-  
+  }  
   auto insideHalfSpace = (m_HSNorm.Dot(p-m_HSPoint) > 0);
+  
   return (insideSphere && insideHalfSpace);
 } // SphericalSurfaceWithHalfSpace::IsInside()
